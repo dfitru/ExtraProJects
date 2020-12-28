c39b6dc83258f26f5e4501fce27ddd69a2a98ffe
Fitru <dfitru@structurepoint.com>
2020-12-28 15:25:51 -0500

    Outing Commit

---
+++ OutingsClass/Outings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutingsClass
+{
+    public enum EventType
+    {
+        Golf = 1,
+        Bowling,
+        AmusingPark,
+        Concert
+    }
+    public class Outings
+    {
+       
+        public int NumberOfPeople { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalCostPerPersonPerEvent { get; set; }
+        public decimal TotalCostForEvent { get; set; }
+        public EventType EventTypes { get; set; }
+        public Outings() { }
+        public Outings(int peopleNumber,DateTime date,decimal costPerpersoneEvent,decimal eventCost,EventType eventType ) 
+        { 
+            NumberOfPeople = peopleNumber;
+            Date = date;
+            TotalCostPerPersonPerEvent = costPerpersoneEvent;
+            TotalCostForEvent = eventCost;
+            EventTypes = eventType;
+        }
+    }
+}
